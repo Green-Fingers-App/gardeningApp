0f93d4a92ccd523356054dfef3cdb6f667973634
CasimirG12 <casimirbomans@gmail.com>
2024-10-15 09:57:49 +0200

    ceate root navigation app, closes issue #33

---
+++ green-fingers/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <Link href={"/profile/home"}>Go To User Page</Link>
+    </View>
+  );
+};
+
+export default index;
---
+++ green-fingers/app/profile/home.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const home = () => {
+  return (
+    <View>
+      <Text>home</Text>
+    </View>
+  );
+};
+
+export default home;
+
+const styles = StyleSheet.create({
+  background: {},
+});
