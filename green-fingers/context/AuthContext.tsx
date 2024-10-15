1185e4db6203e341294f580dccf81618157b41ec
CasimirG12 <casimirbomans@gmail.com>
2024-10-15 13:49:27 +0200

    add auth wrapper for authentication logic

---
+++ green-fingers/context/AuthContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useState, ReactNode, useContext } from "react";
+import { User, AuthContextProps } from "@/types/authtypes";
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an Authprovider");
+  }
+  return context;
+};
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = async (email: string, password: string) => {
+    const userData = await mockAuth(email, password);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const mockAuth = async (email: string, password: string): Promise<User> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (email === "test@example.com" && password === "password") {
+        resolve({ id: 1, email: email, username: "TestUser" });
+      } else {
+        reject({ error: "Invalid Email or Password" });
+      }
+    }, 1000);
+  });
+};
