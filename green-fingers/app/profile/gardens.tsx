cd946e9d0f1437df601b25424c0acb5299f5b775
CasimirG12 <casimirbomans@gmail.com>
2024-10-15 09:57:49 +0200

    ceate root navigation app, closes issue #33

---
+++ green-fingers/app/profile/gardens.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const gardens = () => {
+  return (
+    <View>
+      <Text>gardens</Text>
+    </View>
+  );
+};
+
+export default gardens;
