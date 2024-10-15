76d8c83a77b99871bd2801dbafc4cd73dff9b93e
CasimirG12 <casimirbomans@gmail.com>
2024-10-15 11:18:02 +0200

    create login page, sign up page, forgot password page, closes #34

---
+++ green-fingers/app/(auth)/login.tsx
@@ -0,0 +1,156 @@
+import { Link, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import Input from "../../components/Input";
+
+export default function App() {
+  const [inputValues, setInputvalues] = useState<{
+    email: string;
+    password: string;
+  }>({
+    email: "",
+    password: "",
+  });
+
+  type InputFieldName = keyof typeof inputValues;
+
+  const [inputErrors, setInputErrors] = useState<{
+    [key in InputFieldName]?: string;
+  }>({});
+
+  const handleChange = (name: InputFieldName, value: string): void => {
+    setInputvalues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleErrorMessage = (name: InputFieldName, text?: string): void => {
+    setInputErrors((prevState) => ({
+      ...prevState,
+      [name]: text,
+    }));
+  };
+
+  const validate = async (): Promise<void> => {
+    if (!inputValues.email) {
+      handleErrorMessage("email", "Please enter your email");
+      return;
+    }
+
+    if (!inputValues.password) {
+      handleErrorMessage("password", "Please enter your password");
+      return;
+    }
+
+    if (inputValues.password === "hallo" && inputValues.email === "hallo") {
+      router.push("/profile/home");
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.background, { width: "100%", height: "100%" }]}>
+        <Text style={styles.title}>Green Fingers</Text>
+        <View style={styles.content}>
+          <Text style={styles.contentTitle}>Login</Text>
+          <Input
+            iconName="account"
+            label="Username"
+            placeholder="Enter your username"
+            autoFocus={true}
+            onChangeText={(text) => handleChange("email", text)}
+            error={inputErrors.email}
+            onFocus={() => {
+              handleErrorMessage("email", undefined);
+            }}
+          />
+          <Input
+            iconName="lock-outline"
+            label="Password"
+            placeholder="Enter your password"
+            secureTextEntry={true}
+            onChangeText={(text) => handleChange("password", text)}
+            password
+            error={inputErrors.password}
+            onFocus={() => handleErrorMessage("password", undefined)}
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.loginButton}
+              onPress={() => validate()}
+            >
+              <Text style={{ fontWeight: "bold" }}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.signUpButton}
+              onPress={() => router.push("/(auth)/signup")}
+            >
+              <Text style={{ fontWeight: "bold" }}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+          <Text>Forgot Password?</Text>
+          <Link href={"/(auth)/forgotpassword"}>
+            <Text>Click Here</Text>
+          </Link>
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    width: "100%",
+    gap: 12,
+    opacity: 0.8,
+  },
+  loginButton: {
+    backgroundColor: "#A9A9A9",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+    opacity: 1,
+  },
+  signUpButton: {
+    backgroundColor: "#DEDEDE",
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 8,
+    opacity: 1,
+  },
+  contentTitle: {
+    color: "#BCBCBC",
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 55,
+    color: "#ABABAB",
+    position: "absolute",
+    top: 30,
+  },
+});
--- green-fingers/app/index.tsx
+++ green-fingers/app/index.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import { View, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
 
 const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-      <Link href={"/profile/home"}>Go To User Page</Link>
-    </View>
-  );
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const isAuthenticated = false;
+
+  useEffect(() => {
+    // Simulate checking the authentication status
+    setTimeout(() => {
+      setLoading(false);
+      if (!isAuthenticated) {
+        router.replace("/(auth)/login");
+      } else {
+        router.replace("/profile");
+      }
+    }, 1000); // Simulate a loading delay
+  }, [isAuthenticated]);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 };
 
 export default index;
