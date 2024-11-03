52e617045c606c1053892b97e59e17ec19d74037
CasimirG12 <casimirbomans@gmail.com>
2024-11-03 17:37:35 +0100

    add start accordion component

--- green-fingers/app/plantDetail/[plantId].tsx
+++ green-fingers/app/plantDetail/[plantId].tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { usePlants } from "@/context/PlantsContext";
 import { UserPlant } from "@/types/models";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "@/constants/colors";
+import Accordion from "@/components/Accordion";
+import AccordionItem from "@/components/AccordionItem";
 
 const PlantDetailPage = () => {
   const { plantId } = useLocalSearchParams();
@@ -17,12 +19,13 @@ const PlantDetailPage = () => {
     if (newPlant) {
       setPlant(newPlant);
     }
-  }, []);
+  }, [plantId]); // Add plantId as dependency
+
   return (
     <>
       <Stack.Screen
         options={{
-          title: plant?.plant.name.commonName,
+          title: plant?.plant.name.commonName || "Plant Details", // Fallback title
           headerLeft: () => (
             <MaterialCommunityIcons
               name="arrow-left"
@@ -35,9 +38,19 @@ const PlantDetailPage = () => {
           },
         }}
       />
-      <View>
-        <Text>{plant?.feededDate}</Text>
-      </View>
+      <SafeAreaView>
+        <Accordion>
+          <AccordionItem title="Status">
+            <Text>hello</Text>
+          </AccordionItem>
+          <AccordionItem title="Overview">
+            <Text>bloop plaaaaappppp</Text>
+          </AccordionItem>
+          <AccordionItem title="Sensors">
+            <Text style={{ color: "black" }}>Blap</Text>
+          </AccordionItem>
+        </Accordion>
+      </SafeAreaView>
     </>
   );
 };
