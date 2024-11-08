76ac529c8b00a6aaccf46b91bd47247294a30b7d
CasimirG12 <casimirbomans@gmail.com>
2024-11-08 11:08:27 +0100

    add delete ui

--- green-fingers/components/PlantCard.tsx
+++ green-fingers/components/PlantCard.tsx
@@ -4,68 +4,74 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  Alert,
   Pressable,
+  ActivityIndicator,
 } from "react-native";
 import React from "react";
 import { UserPlant } from "../types/models";
 import colors from "@/constants/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
+import { useDeletePlant } from "@/hooks/useDeletePlant";
 
 interface PlantCardProps {
   plant: UserPlant;
 }
 
 const PlantCard: React.FC<PlantCardProps> = ({ plant }) => {
-  const handlePlantDelete = () => {
-    Alert.alert("Delete Plant", "Do you want to delete this plant?", [
-      { text: "Yes", onPress: () => console.log("Plant deleted") },
-      { text: "Cancel", style: "cancel" },
-    ]);
-  };
+  const { deleting, handleDeletePlant } = useDeletePlant();
 
   return (
     <TouchableOpacity
       style={styles.cardContainer}
       onPress={() => router.push(`/plantDetail/${plant.id}`)}
     >
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text style={styles.commonName}>{plant.plant.name.commonName}</Text>
-
-        {/* Delete Icon */}
-        <Pressable
-          onPress={handlePlantDelete}
-          style={({ pressed }) => [
-            styles.deleteButton,
-            pressed && styles.activeDelete, // Active style on press
-            // Conditionally style the icon based on pressed state
-            {
-              opacity: pressed ? 0.7 : 1,
-              transform: [{ scale: pressed ? 0.9 : 1 }],
-            },
-          ]}
-        >
-          <MaterialCommunityIcons
-            name="delete-outline"
-            size={20}
-            color={"black"} // Change color when pressed
-          />
-        </Pressable>
-      </View>
-      <Text style={styles.scientificName}>
-        {plant.plant.name.scientificName}
-      </Text>
-      <Image width={80} height={80} style={styles.picture} />
-      {plant.moistureLevel === "Too Low" ? (
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-          <MaterialCommunityIcons name="alert-circle" size={15} />
-          <Text>I'm thirsty</Text>
-        </View>
+      {!deleting ? (
+        <>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text style={styles.commonName}>{plant.plant.name.commonName}</Text>
+            <Pressable
+              onPress={() => handleDeletePlant(plant)}
+              style={({ pressed }) => [
+                styles.deleteButton,
+                pressed && styles.activeDelete,
+                {
+                  opacity: pressed ? 0.7 : 1,
+                  transform: [{ scale: pressed ? 0.9 : 1 }],
+                },
+              ]}
+            >
+              <MaterialCommunityIcons
+                name="delete-outline"
+                size={20}
+                color={"black"}
+              />
+            </Pressable>
+          </View>
+          <Text style={styles.scientificName}>
+            {plant.plant.name.scientificName}
+          </Text>
+          <Image width={80} height={80} style={styles.picture} />
+          {plant.moistureLevel === "Too Low" ? (
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 4 }}
+            >
+              <MaterialCommunityIcons name="alert-circle" size={15} />
+              <Text>I'm thirsty</Text>
+            </View>
+          ) : (
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 4 }}
+            >
+              <MaterialCommunityIcons name="check-circle" size={15} />
+              <Text>I'm okay</Text>
+            </View>
+          )}
+        </>
       ) : (
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-          <MaterialCommunityIcons name="check-circle" size={15} />
-          <Text>I'm okay</Text>
+        <View style={{ alignItems: "center" }}>
+          <ActivityIndicator size="small" color="#457D58" />
+          <Text> Deleting... </Text>
         </View>
       )}
     </TouchableOpacity>
