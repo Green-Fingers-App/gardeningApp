df20c2f0be500aaab73b3be58f1bb007774f7aa1
rmoolmandreyer <rmoolmandreyer@gmail.com>
2024-11-11 17:06:39 +0100

    Implement plant management features with Firestore integration and UI enhancements

---
+++ green-fingers/types/plantTypes.ts
@@ -0,0 +1,14 @@
+export interface Plant {
+    name: string;
+    scientific_name?: string;
+    type: string;
+    water_frequency: string;
+    water_amount?: string;
+    temperature?: string;
+    humidity?: string;
+    light?: string;
+    soil_type?: string;
+    fertilizer_type?: string;
+    fertilizer_frequency?: string;
+  }
+  
