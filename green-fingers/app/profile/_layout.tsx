c0b98c082c626cb0dc1bf2bb3ad6afa4ba83cfe9
CasimirG12 <casimirbomans@gmail.com>
2024-11-14 13:30:57 +0100

    Add add-button component

--- green-fingers/app/profile/_layout.tsx
+++ green-fingers/app/profile/_layout.tsx
@@ -3,82 +3,103 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Tabs, useRouter } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
 import colors from "@/constants/colors";
-import React from "react";
+import React, { useState } from "react";
 import { PlantsProvider } from "@/context/GardensAndPlantsContext";
+<<<<<<< HEAD
 import { TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
+=======
+import { StyleSheet, TouchableOpacity } from "react-native";
+>>>>>>> fdb88b5 (Add add-button component)
 import { View, Text } from "react-native";
 import { useAuth } from "@/context/AuthContext";
 
 const ProfileLayout: React.FC = () => {
   const router = useRouter();
   const { user } = useAuth();
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <Tabs
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.primaryDefault,
-        },
-        tabBarActiveTintColor: colors.bgLight,
-        tabBarInactiveTintColor: colors.textPrimary,
-        tabBarStyle: {
-          backgroundColor: colors.primaryDefault,
-        },
-        headerRight: () => (
-          <View>
-            <Text>Hello, {user?.email}</Text>
-            <TouchableOpacity
-              style={{ marginRight: 15 }}
-              onPress={() => router.push("/profilePage")}
-            >
-              <MaterialIcons
-                name="account-circle"
-                size={24}
-                color={colors.bgLight}
-              />
-            </TouchableOpacity>
-          </View>
-        ),
-      }}
-    >
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="home" color={color} size={size} />
+    <>
+      <Tabs
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.primaryDefault,
+          },
+          tabBarActiveTintColor: colors.bgLight,
+          tabBarInactiveTintColor: colors.textPrimary,
+          tabBarStyle: {
+            backgroundColor: colors.primaryDefault,
+          },
+          headerRight: () => (
+            <View>
+              <Text>Hello, {user?.email}</Text>
+              <TouchableOpacity
+                style={{ marginRight: 15 }}
+                onPress={() => router.push("/profilePage")}
+              >
+                <MaterialIcons
+                  name="account-circle"
+                  size={24}
+                  color={colors.bgLight}
+                />
+              </TouchableOpacity>
+            </View>
           ),
         }}
-      />
-      <Tabs.Screen
-        name="gardens"
-        options={{
-          title: "Gardens",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="nature" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="plants"
-        options={{
-          title: "Plants",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="local-florist" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="sensors"
-        options={{
-          title: "Sensors",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="sensors" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="gardens"
+          options={{
+            title: "Gardens",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="nature" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="plants"
+          options={{
+            title: "Plants",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="local-florist" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="sensors"
+          options={{
+            title: "Sensors",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="sensors" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity style={styles.addButton} onPress={() => toggleMenu()}>
+          <MaterialIcons name="add" size={45} />
+        </TouchableOpacity>
+      </View>
+      {menuOpen && (
+        <View>
+          <Text>menu</Text>
+        </View>
+      )}
+    </>
   );
 };
 
@@ -111,5 +132,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 30,
+    transform: [{ translateX: -30 }],
   },
 });
