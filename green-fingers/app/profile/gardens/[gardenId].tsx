167d9400d74b0dde6e49a2577ac3a0d1abe29aa9
DHBW-Malte <mal.opderbeck.23@lehre.mosbach.dhbw.de>
2024-11-20 15:08:13 +0100

    Fixing delete garden missing files

--- green-fingers/app/profile/gardens/[gardenId].tsx
+++ green-fingers/app/profile/gardens/[gardenId].tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useGardensAndPlants } from "@/context/GardensAndPlantsContext";
@@ -6,15 +6,20 @@ import { Garden, UserPlant } from "@/types/models";
 import PlantCard from "@/components/PlantCard";
 import colors from "@/constants/colors";
 import OptionMenu from "@/components/OptionMenu";
+import { useDeleteEntity } from "@/hooks/useDeleteEntity";
+
 const GardenDetailPage = () => {
   const { gardenId } = useLocalSearchParams();
   const { fetchGardenPlants, fetchGardenDetail } = useGardensAndPlants();
   const [plants, setPlants] = useState<UserPlant[] | undefined>(undefined);
   const [garden, setGarden] = useState<Garden | undefined>(undefined);
+  const { deleting, handleDeleteEntity } = useDeleteEntity("Garden");
+
   const options = [
     { label: "Edit", onPress: () => console.log("Edit garden") },
-    { label: "Delete", onPress: () => console.log("Delete garden") },
+    { label: "Delete", onPress: () => garden && handleDeleteEntity({ id: garden.id, name: garden.name }) },
   ]
+
   useEffect(() => {
     const fetchGardenData = async () => {
       try {
@@ -26,10 +31,12 @@ const GardenDetailPage = () => {
         console.error("Error fetching garden details or plants:", error);
       }
     };
+
     if (gardenId) {
       fetchGardenData();
     }
   }, [gardenId]);
+
   return (
     <>
       <Stack.Screen
@@ -39,15 +46,27 @@ const GardenDetailPage = () => {
           headerRight: () => <OptionMenu options={options} />,
         }}
       />
-      <View style={styles.pageContainer}>
-        {plants?.map((plant, index) => (
-          <PlantCard plant={plant} key={index} />
-        ))}
-      </View>
+      {garden && !deleting ? (
+        <View style={styles.pageContainer}>
+          {plants?.map((plant, index) => (
+            <PlantCard plant={plant} key={index} />
+          ))}
+        </View>
+      ) : deleting ? (
+        <View style={{ alignItems: "center" }}>
+          <ActivityIndicator size="small" color="#457D58" />
+          <Text> Deleting... </Text>
+        </View>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+      
     </>
   );
 };
+
 export default GardenDetailPage;
+
 const styles = StyleSheet.create({
   pageContainer: {
     alignItems: "center",
