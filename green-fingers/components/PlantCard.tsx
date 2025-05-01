2d282f8d1727cb83bd39a66af06a77d795fd1575
CasimirG12 <casimirbomans@gmail.com>
2025-05-01 15:20:40 +0200

    remove delete hook, will be handeled by toast

--- green-fingers/components/PlantCard.tsx
+++ green-fingers/components/PlantCard.tsx
@@ -5,67 +5,55 @@ import {
   Image,
   TouchableOpacity,
   Pressable,
-  ActivityIndicator,
 } from "react-native";
 import React from "react";
 import { CatalogPlant, UserPlant } from "../types/models";
 import colors from "@/constants/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useDeletePlant } from "@/hooks/useDeletePlant";
 import { shouldBeWatered } from "@/utils/plant";
+import { useGardensAndPlants } from "@/context/GardensAndPlantsContext";
 
 interface PlantCardProps extends React.ComponentProps<typeof TouchableOpacity> {
   plant: UserPlant | CatalogPlant;
 }
 
 const PlantCard: React.FC<PlantCardProps> = ({ plant, ...props }) => {
-  const { deleting, handleDeletePlant } = useDeletePlant();
   const thirsty = shouldBeWatered(plant as UserPlant);
+  const { deleteUserPlant } = useGardensAndPlants();
 
   return (
-    <TouchableOpacity {...props} style={styles.cardContainer}>
-      {!deleting ? (
-        <>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <Text style={styles.commonName}>
-              {"nickName" in plant ? plant.nickName : plant.name.commonName}
-            </Text>
-            {"nickName" in plant && (
-              <Pressable
-                onPress={() => handleDeletePlant(plant)}
-                style={({ pressed }) => [
-                  styles.deleteButton,
-                  pressed && styles.activeDelete,
-                  {
-                    opacity: pressed ? 0.7 : 1,
-                    transform: [{ scale: pressed ? 0.9 : 1 }],
-                  },
-                ]}
-              >
-                <MaterialCommunityIcons
-                  name="delete-outline"
-                  size={20}
-                  color={"black"}
-                />
-              </Pressable>
-            )}
-          </View>
+    <TouchableOpacity {...props} style={{ width: "95%" }}>
+      <View style={styles.cardContainer}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={styles.commonName}>
+            {"nickName" in plant ? plant.nickName : plant.name.commonName}
+          </Text>
+          {"nickName" in plant && (
+            <Pressable
+              onPress={() => deleteUserPlant(plant.id)}
+              style={({ pressed }) => [
+                styles.deleteButton,
+                pressed && styles.activeDelete,
+                {
+                  opacity: pressed ? 0.7 : 1,
+                  transform: [{ scale: pressed ? 0.9 : 1 }],
+                },
+              ]}
+            >
+              <MaterialCommunityIcons
+                name="delete-outline"
+                size={20}
+                color={"black"}
+              />
+            </Pressable>
+          )}
+        </View>
+        <View>
           <Text style={styles.scientificName}>
             {"nickName" in plant
               ? plant.name.commonName
               : plant.name.scientificName}
           </Text>
-          <Image
-            source={{ uri: plant.imageUrl }}
-            width={80}
-            height={80}
-            style={styles.picture}
-          />
           {"nickName" in plant && thirsty ? (
             <View
               style={{ flexDirection: "row", alignItems: "center", gap: 4 }}
@@ -83,13 +71,14 @@ const PlantCard: React.FC<PlantCardProps> = ({ plant, ...props }) => {
               </View>
             )
           )}
-        </>
-      ) : (
-        <View style={{ alignItems: "center" }}>
-          <ActivityIndicator size="small" color="#457D58" />
-          <Text> Deleting... </Text>
         </View>
-      )}
+      </View>
+      <Image
+        source={{ uri: plant.imageUrl }}
+        width={80}
+        height={80}
+        style={styles.picture}
+      />
     </TouchableOpacity>
   );
 };
@@ -99,12 +88,13 @@ export default PlantCard;
 const styles = StyleSheet.create({
   cardContainer: {
     backgroundColor: colors.bgCard,
-    width: "95%",
     position: "relative",
     height: 96,
+    paddingTop: 8,
     paddingLeft: 8,
     borderRadius: 8,
-    justifyContent: "center",
+    flexDirection: "column",
+    width: "100%",
   },
   commonName: {
     fontWeight: "bold",
