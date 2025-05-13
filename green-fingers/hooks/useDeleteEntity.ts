05e6ff71d24b9a75f10ffae9f6e8b6b768ee915c
CasimirG12 <casimirbomans@gmail.com>
2025-05-13 10:58:51 +0200

    replace hook that is still in use

---
+++ green-fingers/hooks/useDeleteEntity.ts
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+import { useGardensAndPlants } from "@/context/GardensAndPlantsContext";
+import { useRouter } from "expo-router";
+
+type DeleteEntityHook = {
+  handleDeleteEntity: (entity: { id: number; name: string }) => void;
+  deleting: boolean;
+};
+
+export const useDeleteEntity = (
+  entityType: "Plant" | "Garden"
+): DeleteEntityHook => {
+  const [deleting, setDeleting] = useState(false);
+  const { deleteUserPlant, deleteUserGarden } = useGardensAndPlants();
+  const router = useRouter();
+
+  const deleteFunctions = {
+    Plant: deleteUserPlant,
+    Garden: deleteUserGarden,
+  };
+
+  const handleDeleteEntity = (entity: { id: number; name: string }) => {
+    console.log("About to delete: ", entity);
+    Alert.alert(
+      `Delete ${entityType}`,
+      `Do you want to delete the ${entityType.toLowerCase()}: ${
+        entity.name
+      }, and everything it contains?`,
+      [
+        {
+          text: "Yes",
+          onPress: async () => {
+            try {
+              console.log(`${entityType} ${entity.name} deleted`);
+              setDeleting(true);
+              deleteFunctions[entityType](entity.id);
+            } catch (error) {
+              Alert.alert(
+                "Error",
+                `Something went wrong while deleting the ${entityType.toLowerCase()}.`
+              );
+            } finally {
+              setDeleting(false);
+              let path = "";
+              if (entityType === "Plant") {
+                path = "/profile/plants";
+              } else {
+                path = "/profile/gardens";
+              }
+              router.push(path);
+            }
+          },
+        },
+        { text: "Cancel", style: "cancel" },
+      ]
+    );
+  };
+
+  return { handleDeleteEntity, deleting };
+};
